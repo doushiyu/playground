06e8afc50241448b900c88fc4f2e90ce4018551c
chasterma <chasterma@gmail.com>
2017-07-27 21:40:05 -0700

    update all

--- practice1/smallerthan1000k-2.py
+++ practice1/smallerthan1000k-2.py
@@ -8,7 +8,7 @@ import time
 
 def f(intrange):
     n=[]
-    for i in range (1,intrange,10):
+    for i in range(1,intrange,10):
         if (i%7 != 0):
             continue
         elif (i%9 != 0):
@@ -16,24 +16,19 @@ def f(intrange):
         elif (i%8 != 1):
             continue
         else:
-            n.append(i) 
-    if (len(n) == 0):
+            n.append(i)
+    #if len(n) == 0:
+    if not n:
       print "There is no valid integer in the range."
     else:
       print n
       with open ('./aaa.txt', 'a') as fobj:
           tm = time.localtime()
           fobj.write(time.strftime("%a, %d %b %Y %H:%M:%S\n",tm))
-          for i in range (len(n)):
-              fobj.write("%d: %d\n" % (i+1,n[i]))
-              
+          for i in range(len(n)):
+              fobj.write("%d: %d\n" % (i+1, n[i]))
 
 if __name__ == '__main__':
-    a1 = eval (raw_input('input the range:'))
+    a1 = eval(raw_input('input the range:'))
     f(a1)
- 
 
-    
-
-
-    
